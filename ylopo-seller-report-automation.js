144383c81328392b5ae00621c6c211fc51b4124b
Daraphista <81943522+Daraphista@users.noreply.github.com>
2025-09-14 23:02:18 +0800

    Make headless

--- ylopo-seller-report-automation.js
+++ ylopo-seller-report-automation.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false
+    headless: true
   });
   const sessionFile = 'ylopo-session.json';
   const context = await browser.newContext({ storageState: sessionFile });
